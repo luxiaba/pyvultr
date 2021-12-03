ab8db16495338c07d4e7a146623d663b6de956e4
fishermanadg <fishermanadg@gmail.com>
2021-12-03 15:08:59 +0800

    upgrade to 0.1.3, add cli support

--- pyvultr/main.py
+++ pyvultr/main.py
@@ -30,7 +30,7 @@ from pyvultr.v2 import (
 __author__ = "fishermanadg"
 __copyright__ = "Copyright 2021, The pyvultr Project"
 __license__ = "License: MIT"
-__version__ = "0.1.2"
+__version__ = "0.1.3"
 __email__ = "fishermanadg@gmail.com"
 
 
